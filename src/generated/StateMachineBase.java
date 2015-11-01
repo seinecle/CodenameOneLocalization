7355688c60717ccb3895b152972bf4cd1e617504
LEVALLOIS <LEVALLOIS@PRT19148.home.em-lyon.priv>
2015-11-01 21:44:54 +0100

    initial push

---
+++ src/generated/StateMachineBase.java
@@ -0,0 +1,514 @@
+/**
+ * This class contains generated code from the Codename One Designer, DO NOT MODIFY!
+ * This class is designed for subclassing that way the code generator can overwrite it
+ * anytime without erasing your changes which should exist in a subclass!
+ * For details about this file and how it works please read this blog post:
+ * http://codenameone.blogspot.com/2010/10/ui-builder-class-how-to-actually-use.html
+*/
+package generated;
+
+import com.codename1.ui.*;
+import com.codename1.ui.util.*;
+import com.codename1.ui.plaf.*;
+import java.util.Hashtable;
+import com.codename1.ui.events.*;
+
+public abstract class StateMachineBase extends UIBuilder {
+    private Container aboutToShowThisContainer;
+    /**
+     * this method should be used to initialize variables instead of
+     * the constructor/class scope to avoid race conditions
+     */
+    /**
+    * @deprecated use the version that accepts a resource as an argument instead
+    
+**/
+    protected void initVars() {}
+
+    protected void initVars(Resources res) {}
+
+    public StateMachineBase(Resources res, String resPath, boolean loadTheme) {
+        startApp(res, resPath, loadTheme);
+    }
+
+    public Container startApp(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Container", com.codename1.ui.Container.class);
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("SpanLabel", com.codename1.components.SpanLabel.class);
+        UIBuilder.registerCustomComponent("ImageViewer", com.codename1.components.ImageViewer.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    if(resPath.endsWith(".res")) {
+                        res = Resources.open(resPath);
+                        System.out.println("Warning: you should construct the state machine without the .res extension to allow theme overlays");
+                    } else {
+                        res = Resources.openLayered(resPath);
+                    }
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        if(res != null) {
+            setResourceFilePath(resPath);
+            setResourceFile(res);
+            initVars(res);
+            return showForm(getFirstFormName(), null);
+        } else {
+            Form f = (Form)createContainer(resPath, getFirstFormName());
+            initVars(fetchResourceFile());
+            beforeShow(f);
+            f.show();
+            postShow(f);
+            return f;
+        }
+    }
+
+    protected String getFirstFormName() {
+        return "Main";
+    }
+
+    public Container createWidget(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Container", com.codename1.ui.Container.class);
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("SpanLabel", com.codename1.components.SpanLabel.class);
+        UIBuilder.registerCustomComponent("ImageViewer", com.codename1.components.ImageViewer.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    res = Resources.openLayered(resPath);
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        return createContainer(resPath, "Main");
+    }
+
+    protected void initTheme(Resources res) {
+            String[] themes = res.getThemeResourceNames();
+            if(themes != null && themes.length > 0) {
+                UIManager.getInstance().setThemeProps(res.getTheme(themes[0]));
+            }
+    }
+
+    public StateMachineBase() {
+    }
+
+    public StateMachineBase(String resPath) {
+        this(null, resPath, true);
+    }
+
+    public StateMachineBase(Resources res) {
+        this(res, null, true);
+    }
+
+    public StateMachineBase(String resPath, boolean loadTheme) {
+        this(null, resPath, loadTheme);
+    }
+
+    public StateMachineBase(Resources res, boolean loadTheme) {
+        this(res, null, loadTheme);
+    }
+
+    public com.codename1.ui.Button findButton1(Component root) {
+        return (com.codename1.ui.Button)findByName("Button1", root);
+    }
+
+    public com.codename1.ui.Button findButton1() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("Button1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("Button1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel(Component root) {
+        return (com.codename1.ui.Label)findByName("Label", root);
+    }
+
+    public com.codename1.ui.Label findLabel() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel1(Component root) {
+        return (com.codename1.ui.Label)findByName("Label1", root);
+    }
+
+    public com.codename1.ui.Label findLabel1() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer1(Component root) {
+        return (com.codename1.ui.Container)findByName("Container1", root);
+    }
+
+    public com.codename1.ui.Container findContainer1() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel3(Component root) {
+        return (com.codename1.ui.Label)findByName("Label3", root);
+    }
+
+    public com.codename1.ui.Label findLabel3() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label3", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label3", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.components.SpanLabel findSpanLabel(Component root) {
+        return (com.codename1.components.SpanLabel)findByName("SpanLabel", root);
+    }
+
+    public com.codename1.components.SpanLabel findSpanLabel() {
+        com.codename1.components.SpanLabel cmp = (com.codename1.components.SpanLabel)findByName("SpanLabel", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.components.SpanLabel)findByName("SpanLabel", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel2(Component root) {
+        return (com.codename1.ui.Label)findByName("Label2", root);
+    }
+
+    public com.codename1.ui.Label findLabel2() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label2", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label2", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer2(Component root) {
+        return (com.codename1.ui.Container)findByName("Container2", root);
+    }
+
+    public com.codename1.ui.Container findContainer2() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container2", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container2", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer(Component root) {
+        return (com.codename1.ui.Container)findByName("Container", root);
+    }
+
+    public com.codename1.ui.Container findContainer() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findButton(Component root) {
+        return (com.codename1.ui.Button)findByName("Button", root);
+    }
+
+    public com.codename1.ui.Button findButton() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("Button", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("Button", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findButton2(Component root) {
+        return (com.codename1.ui.Button)findByName("Button2", root);
+    }
+
+    public com.codename1.ui.Button findButton2() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("Button2", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("Button2", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.components.ImageViewer findImageViewer(Component root) {
+        return (com.codename1.components.ImageViewer)findByName("ImageViewer", root);
+    }
+
+    public com.codename1.components.ImageViewer findImageViewer() {
+        com.codename1.components.ImageViewer cmp = (com.codename1.components.ImageViewer)findByName("ImageViewer", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.components.ImageViewer)findByName("ImageViewer", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public static final int COMMAND_MainButton = 1;
+
+    protected boolean onMainButton() {
+        return false;
+    }
+
+    protected void processCommand(ActionEvent ev, Command cmd) {
+        switch(cmd.getId()) {
+            case COMMAND_MainButton:
+                if(onMainButton()) {
+                    ev.consume();
+                    return;
+                }
+                break;
+
+        }
+        if(ev.getComponent() != null) {
+            handleComponentAction(ev.getComponent(), ev);
+        }
+    }
+
+    protected void exitForm(Form f) {
+        if("GUI 1".equals(f.getName())) {
+            exitGUI1(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            exitMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void exitGUI1(Form f) {
+    }
+
+
+    protected void exitMain(Form f) {
+    }
+
+    protected void beforeShow(Form f) {
+    aboutToShowThisContainer = f;
+        if("GUI 1".equals(f.getName())) {
+            beforeGUI1(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            beforeMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void beforeGUI1(Form f) {
+    }
+
+
+    protected void beforeMain(Form f) {
+    }
+
+    protected void beforeShowContainer(Container c) {
+        aboutToShowThisContainer = c;
+        if("GUI 1".equals(c.getName())) {
+            beforeContainerGUI1(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            beforeContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void beforeContainerGUI1(Container c) {
+    }
+
+
+    protected void beforeContainerMain(Container c) {
+    }
+
+    protected void postShow(Form f) {
+        if("GUI 1".equals(f.getName())) {
+            postGUI1(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            postMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void postGUI1(Form f) {
+    }
+
+
+    protected void postMain(Form f) {
+    }
+
+    protected void postShowContainer(Container c) {
+        if("GUI 1".equals(c.getName())) {
+            postContainerGUI1(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            postContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void postContainerGUI1(Container c) {
+    }
+
+
+    protected void postContainerMain(Container c) {
+    }
+
+    protected void onCreateRoot(String rootName) {
+        if("GUI 1".equals(rootName)) {
+            onCreateGUI1();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(rootName)) {
+            onCreateMain();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void onCreateGUI1() {
+    }
+
+
+    protected void onCreateMain() {
+    }
+
+    protected Hashtable getFormState(Form f) {
+        Hashtable h = super.getFormState(f);
+        if("GUI 1".equals(f.getName())) {
+            getStateGUI1(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
+        if("Main".equals(f.getName())) {
+            getStateMain(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
+            return h;
+    }
+
+
+    protected void getStateGUI1(Form f, Hashtable h) {
+    }
+
+
+    protected void getStateMain(Form f, Hashtable h) {
+    }
+
+    protected void setFormState(Form f, Hashtable state) {
+        super.setFormState(f, state);
+        if("GUI 1".equals(f.getName())) {
+            setStateGUI1(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            setStateMain(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void setStateGUI1(Form f, Hashtable state) {
+    }
+
+
+    protected void setStateMain(Form f, Hashtable state) {
+    }
+
+    protected void handleComponentAction(Component c, ActionEvent event) {
+        Container rootContainerAncestor = getRootAncestor(c);
+        if(rootContainerAncestor == null) return;
+        String rootContainerName = rootContainerAncestor.getName();
+        Container leadParentContainer = c.getParent().getLeadParent();
+        if(leadParentContainer != null && leadParentContainer.getClass() != Container.class) {
+            c = c.getParent().getLeadParent();
+        }
+        if(rootContainerName == null) return;
+        if(rootContainerName.equals("Main")) {
+            if("Button1".equals(c.getName())) {
+                onMain_Button1Action(c, event);
+                return;
+            }
+            if("Button".equals(c.getName())) {
+                onMain_ButtonAction(c, event);
+                return;
+            }
+            if("Button2".equals(c.getName())) {
+                onMain_Button2Action(c, event);
+                return;
+            }
+        }
+    }
+
+      protected void onMain_Button1Action(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_ButtonAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_Button2Action(Component c, ActionEvent event) {
+      }
+
+}
